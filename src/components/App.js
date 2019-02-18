4ac6abcf331f034a070968aca9e023dc65605fd0
vladmv96 <vladikas1996@gmail.com>
2019-02-18 18:23:31 +0300

    create app

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+import Login from './Login';
+import SignUp from './SignUp';
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Login} />
+        <Route path='/registration' component={SignUp}/>
+      </Switch>
+      </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './Login.css';
+import axios from 'axios';
+
+class Login extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    login: "",
+    password: "",
+    data: [],
+    count: 1
+  }
+}
+
+  count = () => {
+    let count = this.state.count*2;
+    this.setState({count});
+  }
+
+  handleUserInput = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  getData = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:3001/users",
+    }).then(response => {
+      console.log(response.data)
+      this.setState({data: response.data})
+    }).catch(error => {
+      console.log(error)
+    });
+  }
+
+  signUp = () => {
+    this.props.history.push('/registration')
+  }
+
+  render() {
+
+    return (
+      <div className="login">
+      <button className="testButton" onClick={this.getData}> Test API </button>
+      <div className="form-container">
+        <input className="login" placeholder="Login" type="login" name="login" value={this.state.login} onChange={this.handleUserInput} />
+        <input className="password" placeholder="Password" type="password" name="password" value={this.state.password} onChange={this.handleUserInput} />
+        <div>
+        <button> Войти </button>
+        <button onClick={this.signUp}> Регистрация </button>
+        </div>
+        <p>{this.state.count}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
